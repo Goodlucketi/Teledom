fae6103cd0d846a85d2df0b6dfb902956a60c9e0
unknown <goodlucketim94@gmail.com>
2024-09-13 14:47:44 +0100

    update

--- src/about/AboutHero.jsx
+++ src/about/AboutHero.jsx
@@ -1,12 +1,12 @@
 import heroImage from "../assets/images/about33.png"
 const AboutHero = () => {
     return ( 
-        <header className="about-hero px-10 pt-10  bg-gradient-to-l from-cyan-400 to-blue-700 mb-10">
+        <header className="about-hero px-5 md:pl-20 pt-5 md:pt-10  bg-gradient-to-l from-cyan-400 to-blue-700 mb-10">
             <div className="hero mx-auto w-11/12 flex justify-between items-center">
-            <div className="title w-5/12">
-                <h2 className="text-3xl font-bold text-white">ABOUT US</h2>
+            <div className="title md:w-5/12">
+                <h2 className="text-lg md:text-3xl font-bold text-white">ABOUT US</h2>
             </div>
-                <div className="img w-7/12">
+                <div className="md:img w-6/12">
                     <img src={heroImage} alt="Lady reading a plan" />
                 </div>
             </div>
--- src/about/AboutText.jsx
+++ src/about/AboutText.jsx
@@ -1,9 +1,9 @@
 import DrEkuwem from "../assets/images/Dr_Ekuwem22.png"
 const AboutText = () => {
     return ( 
-        <main className="p-4">
+        <main className="about">
             <div className="w-11/12 p-2 mx-auto">
-                <div className="mission_statement shadow-md bg-white p-10">
+                <div className="mission_statement shadow-md bg-white p-5 md:p-10">
                     <p className="md:text-lg">
                         Teledom International Limited, RC: 337366 is fully incorporated in Nigeria with the Corporate Affairs Commission Abuja to carry out the business of Information and Communication Technology (ICT) facilities provisioning within and outside Nigeria. Teledom International is a member of Teledom Group. The Group also has WebInn Limited and Technology Development Company Limited both which offer other specialized ICT-based services while IT MART (Resources) Limited, which is a One-Stop Infotech Superstore.
                     </p>
@@ -14,10 +14,10 @@ const AboutText = () => {
                     </p>
                 </div>
 
-                <div className="uniqueness shadow-md bg-white p-10 my-10">
+                <div className="uniqueness shadow-md bg-white p-5 md:p-10 my-10">
                     <p className="md:text-lg font-bold">The most prefered ICT solution provider with a digital A+ rating from its clients</p>
 
-                    <ul className="list-disc px-10 md:text-lg my-5">
+                    <ul className="list-disc pl-5 md:px-10 md:text-lg my-5">
                         <li>Nigerian Communications Commission (NCC)</li>
                         <li>National Information Technology Development (NITDA)</li>
                         <li> Central Bank of Nigeria</li>
@@ -31,7 +31,7 @@ const AboutText = () => {
                     <div className="about-ceo md:w-7/12">
                         <h3 className="text-white text-3xl font-bold">Meet the CEO</h3>
 
-                        <p className="text-md text-justify">
+                        <p className="text-md my-5 md:text-justify">
                             Dr. Emmanuel E. Ekuwem B.Sc., M.Sc., M.Sc., PhD, MIEEE, MISOC, a former National President of the Association of Telecommunication Companies of Nigeria (ATCON), former President of Nigeria Internet Group (NIG) is the Founder and CEO of Teledom Group, an indigenous conglomerate company with expertise in Broadband Information and Communications Technology (ICT)  infrastructure development, deployment and software development for the automation of national socio-economic activities to sustainably boost productivity, enhance good corporate governance and engender prosperity and national well-being .  He has tremendous capabilities and versatility in telecom engineering, information technology and telecom peripherals instrumentation.  His vast knowledge, expertise and experience are a resource and guiding tool for both his company and the larger society.
                         </p>
                     </div>
