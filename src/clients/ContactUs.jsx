6efcaab3c131d97e2690494d1c7e586f0a33cfe5
unknown <goodlucketim94@gmail.com>
2025-03-26 13:32:05 +0100

    Update

--- src/clients/ContactUs.jsx
+++ src/clients/ContactUs.jsx
@@ -1,19 +1,31 @@
 const ContactUs = () => {
     return ( 
         <main className="mx-auto md:w-11/12 p-2 md:p-4">
-            <div className="md:w-11/12 mx-auto my-3 p-4">
-                <div className="address my-5 shadow-md p-3 md:p-4 md:w-10/12 mx-auto md:text-lg">
-                    <h3 className="font-bold text-xl my-3">Head Office</h3>
-                    <p>6A & 6B, Sule Abuka crescent Off Opebi Road, Ikeja, Lagos.</p>
-                    <p>Tel: <a className="text-blue-500" href="tel:+2348081131346">+234 (0) 808 113 1346</a>, <a className="text-blue-500" href="tel:+2348083035594">+234 (0) 808 303 5594</a></p>
-                    <p>E-mail: <a href="mailto:info@teledominternational.net">info@teledominternational.net</a></p>
+            <div className="mx-auto my-3 p-4">
+                <div className="address my-5 shadow-md p-3 md:p-4 md:w-11/12 mx-auto md:text-lg md:flex md:justify-between md:gap-5">
+                    <div className="contact">
+                        <h3 className="font-bold text-xl my-3">Head Office</h3>
+                        <p>6A & 6B, Sule Abuka crescent Off Opebi Road, Ikeja, Lagos.</p>
+                        <p>Tel: <a className="text-blue-500" href="tel:+2347066012671">+234 (0) 706 601 2671</a>, <a className="text-blue-500" href="tel:+2348080580288">+234 (0) 808 058 0288</a></p>
+                        <p>E-mail: <a href="mailto:info@teledominternational.net">info@teledominternational.net</a></p>
+                    </div>
+                    <div className="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.4284201274763!2d3.3538852743597745!3d6.593554722346605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b923ed3749fb7%3A0x91362d0a1f7d3b0a!2sSule%20Abuka%20Cresent%2C%20Opebi%2C%20Wasimi%2FOpebi%2FAllen%20101233%2C%20Lagos!5e0!3m2!1sen!2sng!4v1742992222326!5m2!1sen!2sng" width="400" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                    
                 </div>
 
-                <div className="address my-10 shadow-md p-3 md:p-4 md:w-10/12 mx-auto text-lg">
-                    <h3 className="font-bold text-xl my-3">Uyo Branch Office</h3>
-                    <p>Teledom International Limited 24, Akpakpan Street, Off Wellington Bassey Way, Uyo, Akwa Ibom State.</p>
-                    <p>Tel: <a className="text-blue-500" href="tel:+2348081131346">+234 (0) 808 113 1346</a>, <a className="text-blue-500" href="tel:+2348083035594">+234 (0) 808 303 5594</a></p>
-                    <p>E-mail: <a href="mailto:uyo@teledominternational.net">uyo@teledominternational.net</a></p>
+                <div className="address my-10 shadow-md p-3 md:p-4 md:w-11/12 mx-auto text-lg md:flex md:justify-between md:gap-5">
+                    <div className="contact">
+                        <h3 className="font-bold text-xl my-3">Uyo Branch Office</h3>
+                        <p>Teledom International Limited 24, Akpakpan Street, Off Wellington Bassey Way, Uyo, Akwa Ibom State.</p>
+                        <p>Tel: <a className="text-blue-500" href="tel:+2348081131346">+234 (0) 808 113 1346</a>, <a className="text-blue-500" href="tel:+2348083035594">+234 (0) 808 303 5594</a></p>
+                        <p>E-mail: <a href="mailto:uyo@teledominternational.net">uyo@teledominternational.net</a></p>
+                    </div>
+                    <div className="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3974.424383646079!2d7.93140077435258!3d5.034723838672707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x105d57bf1ce251bd%3A0xe15312d23eaf0a35!2s24%20Akpankpan%20St%2C%20Uyo%20520103%2C%20Akwa%20Ibom!5e0!3m2!1sen!2sng!4v1742991680818!5m2!1sen!2sng" width="400" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                    
                 </div>
             </div>
         </main>
--- src/home/navbar.jsx
+++ src/home/navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 import Logo from '../assets/images/TeledomLogo.png'
 import MenuIcon from '@mui/icons-material/Menu';
@@ -21,21 +21,25 @@ const Navbar = () => {
         <header className='sticky top-0 left-0 z-20 shadow-md bg-slate-100'>
             <nav className="mx-auto w-11/12 py-2 flex justify-between items-center">
                 <div className="logo w-5/12 md:w-2/12 p-3">
-                    <Link to="/">
+                    <NavLink to="/">
                         <img src={Logo} alt="Teledom Logo" className='' />
-                    </Link>
+                    </NavLink>
                 </div>
 
                 {/* Links */}
                 <div className="links p-3">
                     <ul className={`${isOpen ? ('block'): ('hidden')} navMenu w-4/6 md:w-full list-none absolute bg-slate-100 right-0 top-16 p-10 md:p-5 h-screen md:relative md:flex items-center md:h-0 md:top-0 md:bg-transparent transition-all duration-500 ease-in-out md:block`}>
-                        <li className="py-2 px-4 mx-2 my-6 font-mono text-lg text-cyan-700 hover:bg-blue-900 rounded-md hover:text-white duration-500 transition-all"><Link to="/">Home</Link> </li>
-                        <li className="py-2 px-4 mx-2 my-6 font-mono text-lg text-cyan-700 hover:bg-blue-900 rounded-md hover:text-white duration-500 transition-all"><Link to="/about">About Us</Link>  </li>
-                        <li className="py-2 px-4 mx-2 my-6 font-mono text-lg text-cyan-700 hover:bg-blue-900 rounded-md hover:text-white duration-500 transition-all"><Link to="/solutions">Solutions</Link> </li>
-                        {/* <li className="py-2 px-4 mx-2 my-10 font-mono text-lg text-cyan-700 hover:bg-blue-900 rounded-md hover:text-white duration-500 transition-all"><Link to="/portfolio">Port Folio</Link> </li> */}
-                        {/* <li className="py-2 px-4 mx-2 my-10 font-mono text-lg text-cyan-700 hover:bg-blue-900 rounded-md hover:text-white duration-500 transition-all"><Link to="/careers">Careers</Link> </li> */}
-                        <li className="py-2 px-4 mx-2 my-6 font-mono text-lg text-cyan-700 hover:bg-blue-900 rounded-md hover:text-white duration-500 transition-all"><Link to="/client">Clientele</Link> </li>
-                        <li className="py-2 px-4 mx-2 my-6 font-mono text-lg text-cyan-700 hover:bg-blue-900 rounded-md hover:text-white duration-500 transition-all"><Link to="/contact">Contact Us</Link> </li>
+                        <NavLink to="/" className={({isActive})=>`py-2 px-4 mx-2 my-6 font-mono text-lg text-cyan-700 rounded-md hover:text-white duration-500 transition-all ${isActive ? "bg-blue-800 text-white":"text-cyan-700 hover:bg-blue-900"}`}><li >Home</li></NavLink> 
+                        
+                        <NavLink to="/about" className={({isActive})=>`py-2 px-4 mx-2 my-6 font-mono text-lg text-cyan-700 rounded-md hover:text-white duration-500 transition-all ${isActive ? "bg-blue-800 text-white":"text-cyan-700 hover:bg-blue-900"}`}><li>About Us</li></NavLink>  
+                        
+                        <NavLink to="/solutions" className={({isActive})=>`py-2 px-4 mx-2 my-6 font-mono text-lg text-cyan-700 rounded-md hover:text-white duration-500 transition-all ${isActive ? "bg-blue-800 text-white":"text-cyan-700 hover:bg-blue-900"}`}><li>Solutions</li></NavLink> 
+                        {/* <li className="py-2 px-4 mx-2 my-10 font-mono text-lg text-cyan-700 hover:bg-blue-900 rounded-md hover:text-white duration-500 transition-all"><NavLink to="/portfolio">Port Folio</NavLink> </li> */}
+                        {/* <li className="py-2 px-4 mx-2 my-10 font-mono text-lg text-cyan-700 hover:bg-blue-900 rounded-md hover:text-white duration-500 transition-all"><NavLink to="/careers">Careers</NavLink> </li> */}
+                        
+                        <NavLink to="/client" className={({isActive})=>`py-2 px-4 mx-2 my-6 font-mono text-lg text-cyan-700 rounded-md hover:text-white duration-500 transition-all ${isActive ? "bg-blue-800 text-white":"text-cyan-700 hover:bg-blue-900"}`}><li>Clientele</li></NavLink> 
+                        
+                        <NavLink to="/contact" className={({isActive})=>`py-2 px-4 mx-2 my-6 font-mono text-lg text-cyan-700 rounded-md hover:text-white duration-500 transition-all ${isActive ? "bg-blue-800 text-white":"text-cyan-700 hover:bg-blue-900"}`}><li>Contact Us</li></NavLink> 
                     </ul>
                     <div className="menu-bar md:hidden">
                         {isOpen ? (
